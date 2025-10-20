cc360215a8528bbf5bb5f11ef4e3b84f93be57ab
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2025-10-20 15:32:55 +0200

    Update GC_based_meta_UKB_AoUv8_MGB_submit.sh

--- DNANexus/PheWAS/GC_based_meta/GC_based_meta_UKB_AoUv8_MGB_submit.sh
+++ DNANexus/PheWAS/GC_based_meta/GC_based_meta_UKB_AoUv8_MGB_submit.sh
@@ -30,3 +30,5 @@ Rscript ./TOPMed_AFib_pipeline/DNANexus/PheWAS/GC_based_meta/GC_based_meta_UKB_A
 
 cd ..
 rm -rf tmp/
+
+dx upload * --destination exome-seq:/sjj/projects/phewas/v2/sum_stats_collected/UKB_AoUv8_MGB_META/
